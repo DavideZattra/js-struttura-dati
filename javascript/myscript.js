a55ae99f8c1791be0f8f8dc6619333d8647a3b30
DavideZattra <d.zattra@gmail.com>
2021-09-08 18:05:01 +0200

    added background img

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -36,28 +36,45 @@ let myMagicCard = { //Card description and caracteristics
 }
 
 let cardHTML = 
-`<img src="${myMagicCard.image}" placeholder=" BloodFireColossusCard ">
-<ul class="card-element">
-    <li>Database id: ${myMagicCard.id}</li>
-    <li>Name: ${myMagicCard.name}</li>
-    <li>Launch cost: ${myMagicCard.manaCost.join(', ')}</li>
-    <li>Total mana cost: ${myMagicCard.totalManaCost}</li>
-    <li>Type: ${myMagicCard.type} ${myMagicCard.subType}</li>
-    <li>Expansion: ${myMagicCard.edition}</li>
-    <li>Rarity: ${myMagicCard.rarity}</li>
-    <li>Card ability: ${myMagicCard.text.ability}
-        <ul>
-            <li>${myMagicCard.text.ability.description}</li>
-            <li>${myMagicCard.text.ability.manaCost}</li>
-            <li>${myMagicCard.text.ability.tappable}</li>
+`<div class="row align-items-center">
+
+    <div class="col-5" id="ms_card-img">
+
+        <img src="${myMagicCard.image}" placeholder=" BloodFireColossusCard ">
+
+    </div>    
+
+    <div class="col-7 text-start" id="ms_card-description">
+
+        <ul class="card-element">
+
+            <!-- <li>Database id: ${myMagicCard.id}</li>-->
+            <li>Name: ${myMagicCard.name}</li>
+            <li>Launch cost: ${myMagicCard.manaCost.join(', ')}</li>
+            <li>Total mana cost: ${myMagicCard.totalManaCost}</li>
+            <li>Type: ${myMagicCard.type} ${myMagicCard.subType}</li>
+            <li>Expansion: ${myMagicCard.edition}</li>
+            <li>Rarity: ${myMagicCard.rarity}</li>
+            <li>Card ability: 
+                <ul class="">
+
+                    <li>${myMagicCard.text.ability.description}</li>
+                    <li>${myMagicCard.text.ability.manaCost}</li>
+                    <li>${myMagicCard.text.ability.tappable}</li>
+
+                </ul>
+            </li>
+            <li>Flavor text: <em>${myMagicCard.text.flavorText}</em></li>
+            <li>Card artist: ${myMagicCard.artist.name} ${myMagicCard.artist.surname}</li>
+            <li>Card Number: ${myMagicCard.number}</li>
+            <li>Card S/C: ${myMagicCard.strenght}/${myMagicCard.constitution}</li>
+            <li>Border color: ${myMagicCard.border}</li>
+
         </ul>
-    </li>
-    <li>Flavor text: <em>${myMagicCard.text.flavorText}</em></li>
-    <li>Card artist: ${myMagicCard.artist.name} ${myMagicCard.artist.surname}</li>
-    <li>Card Number: ${myMagicCard.number}</li>
-    <li>Card S/C: ${myMagicCard.strenght}/${myMagicCard.constitution}</li>
-    <li>Border color: ${myMagicCard.border}</li>
-</ul>
+
+    </div>
+
+</div>
 `
 ;
 
