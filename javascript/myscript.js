4c0bd2cb0f39bb7c5c7ba4a7f526af9193614272
DavideZattra <d.zattra@gmail.com>
2021-09-08 15:57:25 +0200

    changed image and added card object & char

---
+++ javascript/myscript.js
@@ -0,0 +1,35 @@
+let myMagicCard = { //Card description and caracteristics
+
+    name : 'Bloodfire Colossus',
+    manaCost : [ 6, 'R', 'R'],
+    type : 'creature',
+    subType : 'Giant',
+    edition : 9,
+    rarity : 'rare',
+    text : {
+
+        ability :{
+
+            description : 'Sacrifice Bloodfire Colossus: Bloodfire Colossus deals 6 damage to each creature and each player.',
+            manaCost : ['R'],            
+            tappable : false,
+
+        } ,
+        flavorText : 'It took all its strenght to contain the fire within.'
+
+    },
+    artist : {
+
+        id : 45,
+        name : 'Greg',
+        surname : 'Staples',
+        dateOfBirth : '07/22/1980',
+        placeOfBirth : 'Chicago, Illinois'
+
+    },
+    number : 177,
+    strenght : 6,
+    constitution : 6,
+    border : 'black',
+    image: 'img/myCardImage.jpeg'
+}
